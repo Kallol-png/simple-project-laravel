04df3ba8d2594994827c7b8036a31c566c5630ce
Kallol Mallick <kallolm52@gmail.com>
2026-02-11 20:11:56 +0600

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('modules.task.create', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+     {
+       $users = User::pluck('name','id');
+    //    dd($users);
+        return view('modules.task.create', compact('users'));
+   
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+     {
+    //     $this->validate($request, [
+    //     'title' => 'required',
+    // ]);
+     $request->validate([
+        'title' => 'required',
+        'description' => 'required',
+    ]);
+
+        // dd('works');
+        // dd($request->all());
+        // dd($request->input('title'));
+        // dd($request->exists('description'));
+        // dd($request->all());
+        Task::create($request->all());
+        return redirect()->route('task.index');
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
